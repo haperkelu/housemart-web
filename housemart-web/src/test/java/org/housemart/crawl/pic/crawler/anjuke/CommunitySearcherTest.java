ded52af11c4ad5708bd50ee45ca6c1d0065c640a
qinpeng <qinpeng82921@163.com>
2014-02-27 23:58:55 +0800

    refactor

--- housemart-web/src/test/java/org/housemart/crawl/pic/crawler/anjuke/CommunitySearcherTest.java
+++ housemart-web/src/test/java/org/housemart/crawl/pic/crawler/anjuke/CommunitySearcherTest.java
@@ -2,7 +2,7 @@
  * Created on 2013-4-20
  * 
  */
-package org.housemart.pic.crawl.anjuke;
+package org.housemart.crawl.pic.crawler.anjuke;
 
 import org.housemart.crawl.pic.service.ResidencePicCrawlerService;
 import org.housemart.web.context.SpringContextHolder;
