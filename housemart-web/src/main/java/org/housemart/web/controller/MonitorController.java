8c9c8b50eb5f76fa389f5ac21f04678b19add1ae
hapereklu <haperkelu@gmail.com>
2014-04-14 18:37:45 +1000

    update user access profiling

--- housemart-web/src/main/java/org/housemart/web/controller/MonitorController.java
+++ housemart-web/src/main/java/org/housemart/web/controller/MonitorController.java
@@ -12,9 +12,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
 import org.housemart.framework.dao.generic.GenericDao;
 import org.housemart.framework.dao.generic.PaginateObject;
-import org.housemart.framework.web.context.SpringContextHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -36,7 +36,8 @@ public class MonitorController {
 	public String addToFavoriteByHouseAndUser(Model model, Integer page, Integer pageSize) {
 		
 		Map<String, Object> map = new HashMap<String, Object>();
-		PaginateObject		paginateObject	=	userAccessDao.paginate("findUserAccessList", page, pageSize, map);
+		map.put("tag", StringUtils.join(new String[] {"Page Request Context", "Page Load Performance"}, ","));
+		PaginateObject		paginateObject	=	userAccessDao.paginate("findUserAccessList", page, pageSize, map, "findUserAccessListCount");
 		List<Object> list = paginateObject.getResult();
 		if(!CollectionUtils.isEmpty(list)){
 			model.addAttribute("list", list);
