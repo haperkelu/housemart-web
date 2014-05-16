cdb3ed8ca48ace634fc6f1c26c7433c42369fde7
hapereklu <haperkelu@gmail.com>
2014-05-16 16:19:19 +1000

    remove water mask

--- housemart-web/src/main/java/org/housemart/util/PicUtils.java
+++ housemart-web/src/main/java/org/housemart/util/PicUtils.java
@@ -8,12 +8,10 @@ package org.housemart.util;
 public class PicUtils {
   
   private static final String SUFIX_WATER_MASK = "watermark/1/image/aHR0cDovL2hvdXNlbWFydC5xaW5pdWRuLmNvbS93YXRlcm1hcmslMjAwNF8xMzgxNDA0MTg1MTk0LnBuZw==/dissolve/100/gravity/Center";
+  private static boolean ENABLE_WATER_MASK = false;
   
   public static String wrapWaterMask(String picUrl) {
-    String result = picUrl;
-    if (result != null) {
-      result = result + "?" + SUFIX_WATER_MASK;
-    }
-    return result;
+	  return ENABLE_WATER_MASK? (picUrl + "?" + SUFIX_WATER_MASK): picUrl;
   }
+  
 }
