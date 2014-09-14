99355897bb46b5538e70c3052c069665ee8f2a79
qinpeng <qinpeng@WIN-61A3SRE1E8C>
2014-09-14 20:44:02 +0800

    page update

--- housemart-web/src/main/java/org/housemart/crawl/ziprealty/service/DetailPageCrawler.java
+++ housemart-web/src/main/java/org/housemart/crawl/ziprealty/service/DetailPageCrawler.java
@@ -30,101 +30,128 @@ import org.webharvest.runtime.variables.NodeVariable;
 import com.caucho.hessian.client.HessianProxyFactory;
 
 public class DetailPageCrawler extends _ACrawler {
-  
-  private PicUrlCrawler puc = new PicUrlCrawler();
-  private ObjectMapper om = new ObjectMapper();
-  
-  public DetailPageCrawler() {
-    super(_IZrConstants.CRAWLER_CIG_HOUSE_DETAIL, "");
-  }
-  
-  public ZrHouse crawlDetailInfo(String detailUrl) throws JsonGenerationException, JsonMappingException, IOException {
-    ZrHouse zrHouse = new ZrHouse();
-    
-    final List<NodeVariable> title = new ArrayList<NodeVariable>();
-    final List<NodeVariable> block = new ArrayList<NodeVariable>();
-    final List<NodeVariable> mls = new ArrayList<NodeVariable>();
-    final List<NodeVariable> price = new ArrayList<NodeVariable>();
-    final List<NodeVariable> listed = new ArrayList<NodeVariable>();
-    final List<NodeVariable> status = new ArrayList<NodeVariable>();
-    final List<NodeVariable> detail = new ArrayList<NodeVariable>();
-    final List<NodeVariable> neighborhood = new ArrayList<NodeVariable>();
-    
-    Scraper scraper = new HouseMartScraper(config, workDir);
-    scraper.getContext().put("url", detailUrl);
-    scraper.getContext().put("title", title);
-    scraper.getContext().put("block", block);
-    scraper.getContext().put("mls", mls);
-    scraper.getContext().put("price", price);
-    scraper.getContext().put("listed", listed);
-    scraper.getContext().put("status", status);
-    scraper.getContext().put("detail", status);
-    scraper.getContext().put("status", status);
-    scraper.getContext().put("detail", detail);
-    scraper.getContext().put("neighborhood", neighborhood);
-    // scraper.getHttpClientManager().setHttpProxy("localhost", 8888);
-    scraper.setDebug(false);
-    scraper.execute();
-    
-    if (!CollectionUtils.isEmpty(title)) {
-      zrHouse.setTitle(title.get(0).toString().replaceAll("\n", ","));
-      zrHouse.setBlock(block.get(0).toString());
-      zrHouse.setMls(mls.get(0).toString());
-      zrHouse.setPrice(price.get(0).toString());
-      zrHouse.setListed(listed.get(0).toString());
-      zrHouse.setStatus(status.size() > 0 ? status.get(0).toString() : "");
-      zrHouse.setHouseDetail(stripHtml(detail.get(0).toString()));
-      zrHouse.setNeighborhood(neighborhood.size() > 0 ? neighborhood.get(0).toString() : "");
-      zrHouse.setLink(detailUrl);
-      zrHouse.setAddTime(new Date());
-      zrHouse.setUpdateTime(new Date());
-      
-      if (zrHouse.getMls() != null) {
-        List<String> picUrlsOnZr = puc.crawlPicUrls(zrHouse.getMls());
-        List<String> picUrlsOnQn = new ArrayList<String>();
-        if (picUrlsOnZr != null) {
-          PicCrawler pc = SpringContextHolder.getBean("picCrawler");
-          ResourceProvider resourceProvider = SpringContextHolder.getBean("resourceProvider");
-          
-          for (String puOnZr : picUrlsOnZr) {
-            if (StringUtils.isBlank(puOnZr.trim())) {
-              continue;
-            }
-            String photoName = StringUtils.substringAfterLast(puOnZr, "/");
-            String fileSavePath = new SimpleDateFormat("yyyyMMdd").format(new Date()) + _IZrConstants.FILE_SUBPATH_RESIDENCE + "/"
-                + photoName;
-            try {
-              String path = pc.crawl(_IZrConstants.HOST_ZIP_REALTY + puOnZr, fileSavePath);
-              // TODO:uncomments
-              String URL = resourceProvider.getValue("housemart.pic.service.url");
-              HessianProxyFactory factory = new HessianProxyFactory();
-              HessianPicServiceInterface service = (HessianPicServiceInterface) factory.create(HessianPicServiceInterface.class,
-                  URL);
-              PicSaveResult remoteResult = service.savePicToCloud(-1, photoName, "image/jpg", path);
-              if (remoteResult != null && remoteResult.getCode() == 200) {
-                picUrlsOnQn.add(remoteResult.getUrl());
-              }
-            } catch (Exception e) {
-              log.error(e.getMessage(), e);
-            }
-          }
-        }
-        zrHouse.setQnPics(om.writeValueAsString(picUrlsOnQn));
-        zrHouse.setZrPics(om.writeValueAsString(picUrlsOnZr));
-      }
+
+    private PicUrlCrawler puc = new PicUrlCrawler();
+    private ObjectMapper om = new ObjectMapper();
+
+    public DetailPageCrawler() {
+	super(_IZrConstants.CRAWLER_CIG_HOUSE_DETAIL, "");
     }
-    return zrHouse;
-  }
-  
-  private String stripHtml(String content) {
-    // <p>段落替换为换行
-    content = content.replaceAll("<p .*?>", "\r\n");
-    // <br><br/>替换为换行
-    content = content.replaceAll("<br\\s*/?>", "\r\n");
-    // 去掉其它的<>之间的东西
-    content = content.replaceAll("\\<.*?>", "");
-    // 还原HTML
-    // content = HTMLDecoder.decode(content);
-    return content;
-  }
+
+    public ZrHouse crawlDetailInfo(String detailUrl)
+	    throws JsonGenerationException, JsonMappingException, IOException {
+	ZrHouse zrHouse = new ZrHouse();
+
+	final List<NodeVariable> title = new ArrayList<NodeVariable>();
+	final List<NodeVariable> block = new ArrayList<NodeVariable>();
+	final List<NodeVariable> mls = new ArrayList<NodeVariable>();
+	final List<NodeVariable> price = new ArrayList<NodeVariable>();
+	final List<NodeVariable> listed = new ArrayList<NodeVariable>();
+	final List<NodeVariable> status = new ArrayList<NodeVariable>();
+	final List<NodeVariable> detail = new ArrayList<NodeVariable>();
+	final List<NodeVariable> neighborhood = new ArrayList<NodeVariable>();
+	final List<NodeVariable> source = new ArrayList<NodeVariable>();
+
+	Scraper scraper = new HouseMartScraper(config, workDir);
+	scraper.getContext().put("url", detailUrl);
+	scraper.getContext().put("title", title);
+	scraper.getContext().put("block", block);
+	scraper.getContext().put("mls", mls);
+	scraper.getContext().put("price", price);
+	scraper.getContext().put("listed", listed);
+	scraper.getContext().put("status", status);
+	scraper.getContext().put("detail", detail);
+	scraper.getContext().put("neighborhood", neighborhood);
+	scraper.getContext().put("source", source);
+	// scraper.getHttpClientManager().setHttpProxy("localhost", 8888);
+	scraper.setDebug(false);
+	scraper.execute();
+
+	if (!CollectionUtils.isEmpty(title)) {
+	    if (title.size() > 0)
+		zrHouse.setTitle(title.get(0).toString().replaceAll("\n", ","));
+	    if (block.size() > 0)
+		zrHouse.setBlock(block.get(0).toString());
+	    if (mls.size() > 0)
+		zrHouse.setMls(StringUtils.substringAfter(
+			StringUtils.substringBefore(mls.get(0).toString(), ","),
+			"#"));
+	    if (price.size() > 0)
+		zrHouse.setPrice(price.get(0).toString());
+	    if (listed.size() > 0)
+		zrHouse.setListed(listed.get(0).toString());
+	    if (status.size() > 0)
+		zrHouse.setStatus(status.size() > 0 ? status.get(0).toString()
+			: "");
+	    if (detail.size() > 0)
+		zrHouse.setHouseDetail(stripHtml(detail.get(0).toString()));
+	    zrHouse.setNeighborhood(neighborhood.size() > 0 ? neighborhood.get(
+		    0).toString() : "");
+	    zrHouse.setLink(detailUrl);
+	    zrHouse.setAddTime(new Date());
+	    zrHouse.setUpdateTime(new Date());
+	    String src = source.size() == 0 ? "CARETS_OC" : source.get(0)
+		    .toString().trim();
+
+	    if (zrHouse.getMls() != null) {
+		List<String> picUrlsOnZr = puc.crawlPicUrls(zrHouse.getMls(),
+			src);
+		List<String> picUrlsOnQn = new ArrayList<String>();
+		if (picUrlsOnZr != null) {
+		    PicCrawler pc = SpringContextHolder.getBean("picCrawler");
+		    ResourceProvider resourceProvider = SpringContextHolder
+			    .getBean("resourceProvider");
+
+		    for (String puOnZr : picUrlsOnZr) {
+			if (StringUtils.isBlank(puOnZr.trim())) {
+			    continue;
+			}
+			String photoName = StringUtils.substringAfterLast(
+				puOnZr, "/");
+			String fileSavePath = new SimpleDateFormat("yyyyMMdd")
+				.format(new Date())
+				+ _IZrConstants.FILE_SUBPATH_RESIDENCE
+				+ "/"
+				+ photoName;
+			try {
+			    String path = pc.crawl(
+				    _IZrConstants.HOST_ZIP_REALTY + puOnZr,
+				    fileSavePath);
+			    // TODO:uncomments
+			    String URL = resourceProvider
+				    .getValue("housemart.pic.service.url");
+			    HessianProxyFactory factory = new HessianProxyFactory();
+			    HessianPicServiceInterface service = (HessianPicServiceInterface) factory
+				    .create(HessianPicServiceInterface.class,
+					    URL);
+			    PicSaveResult remoteResult = service
+				    .savePicToCloud(-1, photoName, "image/jpg",
+					    path);
+			    if (remoteResult != null
+				    && remoteResult.getCode() == 200) {
+				picUrlsOnQn.add(remoteResult.getUrl());
+			    }
+			} catch (Exception e) {
+			    log.error(e.getMessage(), e);
+			}
+		    }
+		}
+		zrHouse.setQnPics(om.writeValueAsString(picUrlsOnQn));
+		zrHouse.setZrPics(om.writeValueAsString(picUrlsOnZr));
+	    }
+	}
+	return zrHouse;
+    }
+
+    private String stripHtml(String content) {
+	// <p>段落替换为换行
+	content = content.replaceAll("<p .*?>", "\r\n");
+	// <br><br/>替换为换行
+	content = content.replaceAll("<br\\s*/?>", "\r\n");
+	// 去掉其它的<>之间的东西
+	content = content.replaceAll("\\<.*?>", "");
+	// 还原HTML
+	// content = HTMLDecoder.decode(content);
+	return content;
+    }
 }
--- housemart-web/src/main/java/org/housemart/crawl/ziprealty/service/PicUrlCrawler.java
+++ housemart-web/src/main/java/org/housemart/crawl/ziprealty/service/PicUrlCrawler.java
@@ -5,10 +5,10 @@
 package org.housemart.crawl.ziprealty.service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
 import org.housemart.crawl.common.crawler.HouseMartScraper;
 import org.housemart.crawl.common.crawler._ACrawler;
 import org.housemart.crawl.ziprealty._IZrConstants;
@@ -21,14 +21,14 @@ public class PicUrlCrawler extends _ACrawler {
 	super(_IZrConstants.CRAWLER_CIG_PIC_URL, "");
     }
 
-    public List<String> crawlPicUrls(String mls) {
+    public List<String> crawlPicUrls(String mls, String source) {
 	List<String> urls = new ArrayList<String>();
 	final List<NodeVariable> pic = new ArrayList<NodeVariable>();
 	Scraper scraper = new HouseMartScraper(config, workDir);
 	scraper.getContext().put(
 		"url",
 		"http://www.ziprealty.com/xhr/photoserver?listing_num=" + mls
-			+ "&source=CARETS_OC");
+			+ "&source=" + source);
 	scraper.getContext().put("pic", pic);
 	// scraper.getHttpClientManager().setHttpProxy("localhost", 8888);
 	scraper.setDebug(false);
@@ -37,31 +37,10 @@ public class PicUrlCrawler extends _ACrawler {
 	if (!CollectionUtils.isEmpty(pic) && pic.get(0).toString().length() > 0) {
 	    if (!CollectionUtils.isEmpty(pic)) {
 		String[] purls0 = pic.get(0).toString().split("\t\n");
-		String[] purls1 = pic.get(1).toString().split("\t\n");
 		if (purls0 != null) {
-		    urls = Arrays.asList(purls0);
-		}
-		if (purls1 != null) {
-		    urls.addAll(Arrays.asList(purls1));
-		}
-	    }
-	} else {
-	    scraper.getContext().put(
-		    "url",
-		    "http://www.ziprealty.com/xhr/photoserver?listing_num="
-			    + mls + "&source=CABCREIS");
-	    scraper.getContext().put("pic", pic);
-	    scraper.setDebug(false);
-	    scraper.execute();
-
-	    if (!CollectionUtils.isEmpty(pic)) {
-		String[] purls0 = pic.get(0).toString().split("\t\n");
-		String[] purls1 = pic.get(1).toString().split("\t\n");
-		if (purls0 != null) {
-		    urls = Arrays.asList(purls0);
-		}
-		if (purls1 != null) {
-		    urls.addAll(Arrays.asList(purls1));
+		    for (String url : purls0)
+			if (StringUtils.isNotBlank(url))
+			    urls.add(url);
 		}
 	    }
 	}
